e01eabbe99f1acf769c0e12d14cadb030fde4344
SatvikaReddy <63241159+SatvikaReddy@users.noreply.github.com>
2025-04-10 16:21:49 -0400

    Revert "Added the data to the visualization graphs with top 10,000 rows and 35 columns"

--- anime-chart/src/App.js
+++ anime-chart/src/App.js
@@ -1,18 +1,30 @@
-
 import React from "react";
 import "./App.css";
 import AnimeBarChart from "./components/AnimeBarChart";
-import AnimeBubbleChart from "./components/AnimeBubbleChart";
 import GenderRadarChart from "./components/GenderRadarChart";
+import AnimeBubbleChart from "./components/AnimeBubbleChart";
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Anime Dashboard</h1>
-      <div style={{ display: "flex", flexWrap: "wrap", gap: "40px", justifyContent: "center" }}>
-        <AnimeBarChart />
-        <GenderRadarChart />
-        <AnimeBubbleChart />
+    <div style={{ padding: "30px" }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          flexWrap: "wrap",
+          gap: "30px",
+        }}
+      >
+        <div style={{ flex: "1 1 30%", minWidth: "300px" }}>
+          <AnimeBarChart />
+        </div>
+        <div style={{ flex: "1 1 30%", minWidth: "300px" }}>
+          <GenderRadarChart />
+        </div>
+        <div style={{ flex: "1 1 30%", minWidth: "300px" }}>
+         <AnimeBubbleChart />
+        </div>
       </div>
     </div>
   );
